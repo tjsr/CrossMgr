de68327b4fbf50b1249e8a2d3739fe11f92ee0da
Tim Rowe <tim@tjsr.id.au>
2024-09-10 01:40:53 +0000

    Allow changes on any dev/* branch.

--- compile.sh
+++ compile.sh
@@ -444,7 +444,7 @@ tagrepo() {
 
 dorelease() {
 	CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD -- | head -1)
-	if [[ ! "$CURRENT_BRANCH" =~ ^dev ]]; then
+	if [[ ! "$CURRENT_BRANCH" =~ ^dev\/ ]]; then
 		echo "Unable to do release on $CURRENT_BRANCH branch. You must be on dev branch to cut a release".
         exit 1
 	fi
