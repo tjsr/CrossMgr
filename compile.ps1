c81bf159c13db8ed5eccb153852cd3a226ed852a
Tim Rowe <tim@tjsr.id.au>
2025-01-30 14:25:12 +1100

    Oops - condition is inverted.

--- compile.ps1
+++ compile.ps1
@@ -137,7 +137,7 @@ function doPyInstaller($program)
 
 function GetVersion($program)
 {
-	if ( -not [string]::IsNullOrEmpty($program) ) {
+	if ( [string]::IsNullOrEmpty($program) ) {
 		Get-PSCallStack
 		Write-Host "No program specified in GetVersion. Aborting..."
 		exit 1
