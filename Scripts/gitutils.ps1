9ec9d8547d620382871706b3a99adcd1b0dfede4
Tim Rowe <tim@tjsr.id.au>
2025-01-31 09:10:01 +1100

    Debug output to gitutils

--- Scripts/gitutils.ps1
+++ Scripts/gitutils.ps1
@@ -2,23 +2,30 @@
 
 function IsDevelopmentBranch() {
 	$githubref = $env:GITHUB_REF.Split('/')
-	if ($githubref[1].ToLower() -ne 'heads') {
+    $refs = $githubref[1].ToLower()
+	if ($refs -ne 'heads') {
+        Write-Debug "Refs is not heads", $refs
 		return $false
 	}
 	$branchName = $githubref[2]
 
     if ( [string]::IsNullOrEmpty($branchName)) {
+        Write-Debug "Branch name is empty"
         return $false
     }
     if ( $branchName -eq 'dev' ) {
+        Write-Debug "On dev branch"
         return $true
     }
     if ( $branchName.StartsWith('develop/') ) {
+        Write-Debug "On develop/* branch"
         return $true
     }
     if ( $branchName.StartsWith('fix/') ) {
+        Write-Debug "On fix/* branch"
         return $true
     }
+    Write-Debug "Branch name was not recognized", $branchName
 
     return $false
 }
