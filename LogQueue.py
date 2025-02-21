dc4f163aad8f9a41a32f668b1f0dc538800b6aa9
Tim Rowe <tim@tjsr.id.au>
2025-02-21 12:03:39 +1100

    Replace LogQueue calls with calls to new logger.

--- LogQueue.py
+++ LogQueue.py
@@ -4,6 +4,7 @@ from queue import Queue
 from types import TracebackType
 from typing import Type
 
+import Log
 import Utils
 
 class LogQueue:
@@ -20,26 +21,23 @@ class LogQueue:
 
 	def q(self, category: str, message: str):
 		self._q.put((category, message))
-		Utils.writeLog('{}: {}: {}'.format(self._loggerName, category, message))
+		Log.getLogger(self._loggerName).getChild(category).info(message)
 
 	def error(self, category: str, message: str):
 		self._q.put((category, message))
-		Utils.writeLog('ERROR ({}): {}: {}'.format(self._loggerName, category, message))
+		Log.getLogger(self._loggerName).getChild(category).error(message)
 
 	def exception(self,
 	              category: str,
 	              e: Exception,
 	              exc_info: tuple[Type[BaseException], BaseException, TracebackType] | tuple[None, None, None] | None = None
 	) -> list[str]:
+		Log.getLogger(self._loggerName).getChild(category).exception(e, exc_info=exc_info)
 		ex: list[str]
 		if exc_info is None:
 			exc_info = sys.exc_info()
 
 		eType, eValue, eTraceback = exc_info
 		ex = traceback.format_exception(eType, e, eTraceback)
-
-		for d in ex:
-			for line in d.split('\n'):
-				self._q.put((category, line))
 
 		return ex
