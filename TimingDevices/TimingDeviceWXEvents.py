5d341896e54d7407a92d1e47fdcce23341bec74d
Tim Rowe <tim@tjsr.id.au>
2025-02-24 08:17:32 +1100

    Improve call signatures and add signalling and handlet stub of transponder crossing events.

--- TimingDevices/TimingDeviceWXEvents.py
+++ TimingDevices/TimingDeviceWXEvents.py
@@ -2,3 +2,4 @@ import wx.lib.newevent
 
 TimingDeviceDisconnectedEvent, EVT_TIMING_DEVICE_DISCONNECTED = wx.lib.newevent.NewEvent()
 TimingDeviceConnectedEvent, EVT_TIMING_DEVICE_CONNECTED = wx.lib.newevent.NewEvent()
+TimingDeviceTransponderEvent, EVT_TRANSPONDER = wx.lib.newevent.NewEvent()
